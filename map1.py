103b5a31711c990c7235bf94ab876a66bf9db96c
Sam Tredgett <sam.tredgett@gmail.com>
2021-09-08 15:59:36 +0100

    added GeoJson data to the map that is generated to draw polygons around all of the countries, reading data from world.json and using it in the featuregroup application.

--- map1.py
+++ map1.py
@@ -1,11 +1,39 @@
-import folium
-map = folium.Map(location=[51.5074,0.1278], zoom_start=6, tiles="Stamen Terrain")
+import folium, pandas
+
+data_frame = pandas.read_csv('Volcanoes.txt')
+map = folium.Map(location=[37.7749,-122.4914], zoom_start=6, tiles="Stamen Terrain")
+
+# pulling information from the data frame into python list objects for the sake of speed. 
+lat = list(data_frame['LAT'])
+lon = list(data_frame['LON'])
+volcano_names = list(data_frame['NAME'])
+status = list(data_frame['STATUS'])
+elevation = list(data_frame['ELEV'])
+volcano_type = list(data_frame['TYPE'])
+
+def color_decider(elevation:float):
+	'''
+	The color_decider function returns a string to define which colour a marker will be
+	when it is attached to an instance of a volcano in the data set. This is decided against
+	the value of the elevation of the volcano. 
+	'''
+	if elevation in range(0,1500):
+		return 'green'
+	elif elevation in range(1500,3000):
+		return 'orange'
+	else:
+		return 'red'
+
 
 # Generate Feature Group
 fg=folium.FeatureGroup(name="My Map")
 # Iterate through a list of coordinates and plot markers in each location with a popup message
-for coordinates in [[51.5074,0.1278], [50,0]]:
-	fg.add_child(folium.Marker(location=coordinates,popup="I'm a marker!", icon=folium.Icon(color='green')))
+for latitude, longitude, name, stat, elev, volc_type in zip(lat,lon,volcano_names,status,elevation,volcano_type):
+	# Popup string generation
+	popup_info : str = f'This volcano is called {name}, and is a {volc_type} that stands at {elev}m tall. ({stat})'
+	# Add the marker
+	fg.add_child(folium.CircleMarker(location=[latitude,longitude],radius=8.0,popup=popup_info, fill_color=color_decider(elev), fill_opacity=0.8))
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
 
 # Add the featuregroup of markers to map
 map.add_child(fg)
